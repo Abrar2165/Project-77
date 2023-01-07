33f7cbf767122a11e8059c1959b27a8bb454d8dd
Abrar2165 <108695905+Abrar2165@users.noreply.github.com>
2023-01-07 09:36:17 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My First Console Project");
+var first_name="Azhar";
+console.log(first_name);
+var last_name="Abdulaziz";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a = 5;
+var b = 10;
+var power=Math.pow(b, a);
+console.log(power);
+var add = a + b;
+console.log(add);
